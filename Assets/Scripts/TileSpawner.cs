6f415329af44c19bf48443f8c5c2894c05482c4d
newmat123 <37107989+newmat123@users.noreply.github.com>
2020-03-09 12:26:52 +0100

    procedual generation start

---
+++ Assets/Scripts/TileSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpawner : MonoBehaviour
+{
+
+    public GameObject[] Tiles;
+
+    private void Start()
+    {
+        int rand = Random.Range(0, Tiles.Length);
+        Instantiate(Tiles[rand], transform.position, Quaternion.identity);
+    }
+
+}
