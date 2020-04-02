d10b92b78505eddf3ea6999b8784c20e7373fc53
Agger0897 <38247158+Agger0897@users.noreply.github.com>
2020-04-02 12:26:29 +0200

    Added AI movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,22 +4,63 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public int health;
-    private int currentHealth;
-    public float speed;
+    public int health;              //Modstanderens standard mængde liv
+    private int currentHealth;      //Modstanderens nuværende liv
+    public float speed;             //Modstanderens hastighed
 
-    private Animator anim;
+    private bool hasTarget = false; // checker om modstanderen har et target
+    private GameObject target;      // Modstanderens target
+
+    private Animator anim;          //Modstanderens animator
+    private Rigidbody2D rb;
 
     void Start()
     {
-        anim = GetComponent<Animator>();
-        //Sætter modstanderens liv til variablen healths værdi
-        currentHealth = health;
+        anim = GetComponent<Animator>();     //Henter modstanderens animator, så komponentet kan ændres
+        currentHealth = health;              //Sætter modstanderens liv til variablen healths værdi
+        rb = GetComponent<Rigidbody2D>();    //Henter modstanderens Rigidbody, så komponentet kan ændres
     }
 
     void Update()
     {
-        
+        if (hasTarget)
+        {
+            //get distance between me and my target
+            float distance = Vector3.Distance(transform.position, target.transform.position);
+            // am I further than 2 units away
+            if (distance > 2)
+            {
+                // I am over 2 units away
+                follow(target.transform); // do a follow
+            }
+        }
+    }
+    
+    // if anything starts to collide with me I will run this method
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {    // is the other object the player
+            target = collision.gameObject;      // it is so set him as my target
+            hasTarget = true;   // I have a target
+        }
+    }
+
+    // if something is no longer coliiding with me I will run this code
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            target = null;
+            hasTarget = false;
+            rb.velocity = new Vector2(0,0);
+        }
+    }
+
+    private void follow(Transform target)
+    {
+        // add force to my rigid body to make me move
+        rb.velocity = (target.transform.position - transform.position).normalized * speed;
     }
 
     public void TakeDamage(int damage)
