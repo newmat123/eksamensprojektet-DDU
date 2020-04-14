a5136119def081c1e3a9bfb511d2bd3491c6bac4
Agger0897 <38247158+Agger0897@users.noreply.github.com>
2020-04-14 14:54:30 +0200

    Added basic camera shake

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(Shake(0.1f, 0.08f));
+        }
+    }
+
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector2 orignalPosition = transform.position;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.position = new Vector2(orignalPosition.x + x,orignalPosition.y + y);
+            elapsed += Time.deltaTime;
+            yield return 0;
+        }
+        transform.position = orignalPosition;
+    }
+}
