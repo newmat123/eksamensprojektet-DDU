821689b5c0e2becd6dd53baaddfc3f3ff83937ac
newmat123 <37107989+newmat123@users.noreply.github.com>
2020-05-05 12:27:51 +0200

    background script changes

--- Assets/Scripts/Map/Background.cs
+++ Assets/Scripts/Map/Background.cs
@@ -6,13 +6,9 @@ public class Background : MonoBehaviour
 {
 
     [SerializeField] private Vector2 parallaxEffectMultiplier;
-    //[SerializeField] private bool infinytiX;
-    //[SerializeField] private bool infinitiY;
 
     private Transform cameraTrans;
     private Vector3 lastCamPos;
-    private float textureSizeX;
-    private float textureSizeY;
 
 
     void Start()
@@ -21,8 +17,6 @@ public class Background : MonoBehaviour
         lastCamPos = cameraTrans.position;
         Sprite sprite = GetComponent<SpriteRenderer>().sprite;
         Texture2D texture = sprite.texture;
-        textureSizeX = texture.width / sprite.pixelsPerUnit;
-        textureSizeY = texture.width / sprite.pixelsPerUnit;
     }
 
 
@@ -31,23 +25,5 @@ public class Background : MonoBehaviour
         Vector3 deltaMovement = cameraTrans.position - lastCamPos;
         transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
         lastCamPos = cameraTrans.position;
-
-        /*
-        if (infinytiX) { 
-            if (Mathf.Abs(cameraTrans.position.x - transform.position.x) >= textureSizeX)
-            {
-                float offsetPositionX = (cameraTrans.position.x - transform.position.x) % textureSizeX;
-                transform.position = new Vector3(cameraTrans.position.x + offsetPositionX, transform.position.y);
-            }
-        }
-        if (infinitiY)
-        {
-            if (Mathf.Abs(cameraTrans.position.y - transform.position.y) >= textureSizeX)
-            {
-                float offsetPositionY = (cameraTrans.position.y - transform.position.y) % textureSizeY;
-                transform.position = new Vector3(cameraTrans.position.x, transform.position.y + offsetPositionY);
-            }
-        }
-        */
     }
 }
