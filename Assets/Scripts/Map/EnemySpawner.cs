393cb26b77ae602597d477bd8e8a8569d4febd47
newmat123 <37107989+newmat123@users.noreply.github.com>
2020-05-12 12:32:17 +0200

    basic enemy spawn

--- Assets/Scripts/Map/EnemySpawner.cs
+++ Assets/Scripts/Map/EnemySpawner.cs
@@ -4,17 +4,51 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
+
+    public GameObject[] Enemys;
+
+    private Vector3 startPoint;
+    public Vector3 endPoint;
+
+    public float upMove = 10f;
+
+    public int enemysToSpawn = 3;
+
+    public LayerMask obj;
+
+
+
     void Start()
     {
-        
+        startPoint = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void SpawnEnemys()
     {
-        
+        if (transform.position.y < endPoint.y)
+        {
+            SpawnEnemys();
+        }
+
+        Vector2 up = new Vector2(transform.position.x, upMove);
+        transform.position = up;
+
+        for(int i = 0; i < enemysToSpawn; i++)
+        {
+            float x = Random.Range(startPoint.x, endPoint.x);
+            Vector2 spawnPoint = new Vector2(x, transform.position.y);
+
+            transform.position = spawnPoint;
+
+            Collider2D objectDetction = Physics2D.OverlapCircle(transform.position, 1, obj);
+            if (objectDetction = null)
+            {
+                int rand = Random.Range(0, Enemys.Length);
+                Instantiate(Enemys[rand]);
+            }
+        }
+
     }
 
 }
